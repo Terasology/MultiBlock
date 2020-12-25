cd0f3a84a7f3799df4bc3b2592269bdeec7e5b9c
Tobias Nett <skaldarnar@googlemail.com>
2020-12-25 12:15:12 +0100

    feat(BlockRegion): adjust for changes to BlockRegion (#13)

--- src/main/java/org/terasology/multiBlock/recipe/LayeredMultiBlockFormItemRecipe.java
+++ src/main/java/org/terasology/multiBlock/recipe/LayeredMultiBlockFormItemRecipe.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.multiBlock.recipe;
 
 import com.google.common.base.Predicate;
@@ -33,7 +20,6 @@ import org.terasology.world.WorldProvider;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockComponent;
 import org.terasology.world.block.BlockRegion;
-import org.terasology.world.block.BlockRegions;
 import org.terasology.world.block.entity.placement.PlaceBlocks;
 import org.terasology.world.block.regions.BlockRegionComponent;
 
@@ -171,8 +157,7 @@ public class LayeredMultiBlockFormItemRecipe implements MultiBlockFormItemRecipe
             }
         }
 
-        BlockRegion multiBlockRegion = BlockRegions.encompassing(new org.joml.Vector3i(minX, lastLayerYDown, minZ),
-                new org.joml.Vector3i(maxX, lastLayerYUp, maxZ));
+        BlockRegion multiBlockRegion = new BlockRegion(minX, lastLayerYDown, minZ).union(maxX, lastLayerYUp, maxZ);
 
         if (callback != null) {
             Map<org.joml.Vector3i, Block> replacementMap = callback.getReplacementMap(multiBlockRegion, layerHeights);
