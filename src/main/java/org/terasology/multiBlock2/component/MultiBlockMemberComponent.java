2cc3cac74f0ebc059a5faca556752f06acaf3f9a
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 17:35:43 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#27)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/multiBlock2/component/MultiBlockMemberComponent.java
+++ src/main/java/org/terasology/multiBlock2/component/MultiBlockMemberComponent.java
@@ -1,29 +1,16 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.multiBlock2.component;
 
 import org.joml.Vector3i;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.world.block.ForceBlockActive;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Not for external use!
  */
 @ForceBlockActive
-public class MultiBlockMemberComponent implements Component {
+public class MultiBlockMemberComponent implements Component<MultiBlockMemberComponent> {
     private Vector3i mainBlockLocation;
 
     public MultiBlockMemberComponent() {
@@ -35,5 +22,10 @@ public class MultiBlockMemberComponent implements Component {
 
     public Vector3i getMainBlockLocation() {
         return mainBlockLocation;
+    }
+
+    @Override
+    public void copyFrom(MultiBlockMemberComponent other) {
+        this.mainBlockLocation = new Vector3i(other.mainBlockLocation);
     }
 }
